9b1de97d71fa024bf4da8eede3b9703e950c6ddf
Jander Gustavo <jander.gustavo@gmail.com>
2023-05-02 22:46:54 -0300

    Criando animação de loading

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Animacao from "./components/Animacao.js";
+import "./App.css";
+
+function App() {
+  return (
+    <div id="principal">
+      <Animacao />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Animacao.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../styles/Animacao.css";
+
+function Animacao() {
+  return (
+    <div id="container">
+      <div>L</div>
+      <div>O</div>
+      <div>A</div>
+      <div>D</div>
+      <div>I</div>
+      <div>N</div>
+      <div>G</div>
+    </div>
+  );
+}
+
+export default Animacao;
